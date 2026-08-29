b6b84e66cedf0c6150fdca1cbd95cb3530dd2f3f
stefan_s <stefan.home@len.xub>
2026-08-29 17:42:10 +0200

    erster kommit -> leeres rust-projekt angelegt um doppelte ordnerstruktur zu vermeiden im leeren git-repo das projekt mit cargo erstellen lassen 	cargo . 	cargo init vim als editor in git gesetzt 	git config --global core.editor "vim"

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Hello, world!");
+    println!("hier wird mir RUST die nächsten korrekten lottozahlen zufällig generieren");
+    println!("💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵💵");
+    println!("💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰💰");
+}
